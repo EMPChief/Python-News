0b4558aef31ac014985bac5dd3ab220d23398381
empchief <bjornmagnekristensen1999@gmail.com>
2024-01-27 21:15:18 +0100

    Refactor news API integration and email content

--- main.py
+++ main.py
@@ -2,26 +2,38 @@ import requests
 import os
 from utility import EmailSender
 
-
 def main():
     email_sender = EmailSender()
-    API_KEY = os.environ.get("API_KEY")
+    api_key = os.environ.get("NEWS_API_KEY")
+
     print("Sending email...")
 
-    news_api_url = f"https://newsapi.org/v2/top-headlines?country=no&apiKey={API_KEY}"
+    news_api_url = f"https://newsapi.org/v2/top-headlines?country=no&apiKey={api_key}"
     response = requests.get(news_api_url)
 
     if response.status_code == 200:
-        news_content = response.content.decode('utf-8')
+        news_content_list = []
+
+        news_data = response.json()
+        for article in news_data['articles']:
+            article_details = (
+                f"Title: {article['title']}\n"
+                f"Description: {article['description']}\n"
+                f"URL: {article['url']}\n"
+                f"Published at: {article['publishedAt']}\n"
+            )
+            news_content_list.append(article_details)
+
+        combined_content = "\n".join(news_content_list)
+
         try:
-            email_sender.send_email("News API Content", news_content)
+            email_sender.send_email("Top News Headlines", combined_content)
             print("Email with News API content sent successfully!")
         except Exception as e:
             print(f"Error sending email: {e}")
-    else:
-        print(
-            f"News API request failed with status code {response.status_code}")
 
+    else:
+        print(f"News API request failed with status code {response.status_code}")
 
 if __name__ == "__main__":
     main()
