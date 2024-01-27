fbfbe0f820d9d0153fc94130abed92ae1f0ac565
empchief <bjornmagnekristensen1999@gmail.com>
2024-01-27 21:40:11 +0100

    Update news API country to US

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from utility import EmailSender
 
 def main():
     email_sender = EmailSender()
-    api_key = os.environ.get("NEWS_API_KEY")
+    api_key = os.environ.get("API_KEY")
 
     print("Sending email...")
 
-    news_api_url = f"https://newsapi.org/v2/top-headlines?country=no&apiKey={api_key}"
+    news_api_url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}"
     response = requests.get(news_api_url)
 
     if response.status_code == 200:
@@ -20,7 +20,7 @@ def main():
                 f"Title: {article['title']}\n"
                 f"Description: {article['description']}\n"
                 f"URL: {article['url']}\n"
-                f"Published at: {article['publishedAt']}\n"
+                f"Published at: {article['publishedAt']}" + 2*"\n"
             )
             news_content_list.append(article_details)
 
