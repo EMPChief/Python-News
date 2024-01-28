66292f99e36e27a722ee16bc1b2f2339443312db
empchief <bjornmagnekristensen1999@gmail.com>
2024-01-28 14:11:56 +0100

    Update sleep time before sending email

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 if __name__ == "__main__":
     news_emailer = NewsEmailer()
     news_emailer.get_top_headlines(country="no", language="no", subject="Top News Headlines in Norway")
-    print("Sleeping for 10 seconds before sending the next email...")
+    print("Sleeping for 60 seconds before sending the next email...")
     time.sleep(60)
     news_emailer.get_top_headlines(country="us", language="en", subject="Top News Headlines in United States")
     print("Script finished.")
