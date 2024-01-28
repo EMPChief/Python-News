537ac55b064f834d2162144df2bbfc2ce896525b
empchief <bjornmagnekristensen1999@gmail.com>
2024-01-28 14:07:09 +0100

    Add delay between email sends

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from utility import NewsEmailer
-
+import time
 if __name__ == "__main__":
     news_emailer = NewsEmailer()
     news_emailer.get_top_headlines(country="no", language="no", subject="Top News Headlines in Norway")
+    print("Sleeping for 10 seconds before sending the next email...")
+    time.sleep(60)
     news_emailer.get_top_headlines(country="us", language="en", subject="Top News Headlines in United States")
+    print("Script finished.")
